6ae0478dee1b8650219ddaba9bf1802f0462b7c2
xuantrangk54 <xuantrangk54@gmail.com>
2017-02-06 13:19:01 +0700

    TemCityDemo

---
+++ TeamCityDemoProject.Core/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCityDemoProject.Core
+{
+    public class Common
+    {
+        public static string ParseDomain(string url)
+        {
+            return "news.zing.vn";
+        }
+    }
+}
