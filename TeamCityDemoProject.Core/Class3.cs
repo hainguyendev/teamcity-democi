4ef0d8b1e3289375789c0f6d2b5e5181740f6c66
Hai Nguyen <hainguyendev@gmail.com>
2017-02-07 14:09:16 +0700

    up test build project

---
+++ TeamCityDemoProject.Core/Class3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCityDemoProject.Core
+{
+    public class Class3
+    {
+        public static int ReturnClass3(string input)
+        {
+
+        }
+    }
+}
